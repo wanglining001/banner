f72c43983aee9e5e01fa2a354b26edc8f45332cc
wanglining001 <1052185069@qq.com>
2020-07-16 08:02:19 +0800

    first commit

---
+++ src/main/java/cn/kgc/controller/HelloController.java
@@ -0,0 +1,20 @@
+package cn.kgc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author 王立宁
+ * @create 2020-05-09
+ * 类描述：
+ */
+@Controller
+public class HelloController {
+    @RequestMapping("/")
+    @ResponseBody
+    public String start(){
+        return "hello,world";
+    }
+}
